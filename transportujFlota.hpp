8f7e193216ae53d3682e2c8155ab0e2b2e8d276d
Marcinek2008 <ms2408@op.pl>
2025-11-01 21:41:23 +0100

    Update transportujFlota.hpp

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,21 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+	if (towar == 0)
+		return 0;
+
+	Stocznia stocznia{};
+	unsigned int liczbaZaglowcow=0;
+
+	while (Stocznia::getTotalCap() < towar) {
+		Statek* statek = stocznia();
+		statek->transportuj();
+
+		if (dynamic_cast< Zaglowiec* >(statek))
+			++liczbaZaglowcow;
+
+		delete statek;
+	}
+
+	return liczbaZaglowcow;
 }
